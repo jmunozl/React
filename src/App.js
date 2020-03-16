b5dcf66847b7622c69ac855de951635f66c59d29
Jose Muñoz <ext_jomunozl@falabella.cl>
2020-03-16 16:48:13 -0300

    TarjetaFruta

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+import TarjetaFruta from './componentes/TarjetaFruta/';
+
+const App = () => (
+  <div>
+    <TarjetaFruta name={'Sandia'} price={2.0} />
+    <TarjetaFruta name={'Naranga'} price={8.88} />
+    <TarjetaFruta name="Pera" price={3.85} />
+  </div>
+);
 
 export default App;
